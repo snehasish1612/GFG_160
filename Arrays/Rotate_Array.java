433c8b98b6d1468df42181237a9e8fe197bc1343
Snehasish Mukherjee <snehasish1612@gmail.com>
2025-06-19 22:15:26 +0530

    Create Rotate_Array.java

---
+++ Arrays/Rotate_Array.java
@@ -0,0 +1,26 @@
+// User function Template for Java
+
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+        // add your code here
+        int n = arr.length;
+        d = d % n;
+        
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+    
+    static void reverse(int arr[], int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            
+            start++;
+            end--;
+        }
+        
+    }
+}
