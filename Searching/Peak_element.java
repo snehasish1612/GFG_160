b51aca0d865d553e13a8e8e52776bcaad0fc9dfc
Snehasish Mukherjee <snehasish1612@gmail.com>
2025-07-05 23:35:43 +0530

    Create Peak_element.java

---
+++ Searching/Peak_element.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public int peakElement(int[] arr) {
+        // code here
+        int low = 0, high = arr.length - 1;
+        
+        while(low != high){
+            int mid = low + (high - low) / 2;
+            
+            if(arr[mid] > arr[mid + 1]){
+                high = mid;
+            }else{
+                low = mid + 1;
+            }
+        }
+        
+        return low;
+    }
+}
